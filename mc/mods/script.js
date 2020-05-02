d65795f167fa586e324b578b2dbc89c4274ff20e
James <acrodynasty@gmail.com>
2020-05-02 14:59:39 +0100

    added my questline builder

---
+++ mc/mods/script.js
@@ -0,0 +1,128 @@
+
+var QUEST_COUNTER = 0;
+var QUEST_LIST = [];
+var CON_COUNTER = 0;
+var CON_LIST = [];
+
+function addQuestContainer()
+{
+	document.getElementById("Quests").insertAdjacentHTML("afterend","<br>"+
+	"<div id=q"+QUEST_COUNTER+" name='quest' class='row'>q"+
+	QUEST_COUNTER+
+	"<textarea id='name' name='name' rows=1>name</textarea>"+
+	"<textarea id='description' name='description' rows=1>description</textarea>"+
+	
+	"<select id='condition'>"+
+			  "<option value='has'>Has Item</option>"+
+			  "<option value='take'>Take Item</option>"+
+			  "<option value='score'>Has Score Value</option>"+
+			  "<option value='tag'>Has Scoreboard Tag</option>"+
+			  "</select>"+
+	"<textarea id='arguments' name='arguments' rows=1>arguments</textarea>"+
+	"<textarea id='icon' name='icon' rows=1>icon texture location</textarea>"+
+	"<textarea id='hover' name='hover' rows=1>hover text</textarea>"+
+	"<textarea id='require' name='require' rows=1>required quests</textarea>"+
+	"<textarea id='category' name='category' rows=1>category</textarea>"+
+	"</div>");
+	QUEST_LIST.push("q"+QUEST_COUNTER);
+	QUEST_COUNTER++;
+	generateGrid();
+	
+	
+			  
+}
+
+function addConnectorContainer()
+{
+	document.getElementById("Quests").insertAdjacentHTML("afterend","<br>"+
+	"<div id=c"+CON_COUNTER+" name='quest' class='row'>c"+
+	CON_COUNTER+
+	
+	"<select id='icon'>"+
+	"<option value='cquestweaver:textures/gui/arrows/vertical.png'>vertical</option>"+
+	"<option value='cquestweaver:textures/gui/arrows/horizontal.png'>horizontal</option>"+
+	"<option value='cquestweaver:textures/gui/arrows/right-up.png'>right/up</option>"+
+	"<option value='cquestweaver:textures/gui/arrows/left-up.png'>left/up</option>"+
+	"<option value='cquestweaver:textures/gui/arrows/right-down.png'>right/down</option>"+
+	"<option value='cquestweaver:textures/gui/arrows/left-down.png'>left/down</option>"+
+	"</select>"+
+	
+	
+	
+	"<textarea id='require' name='require' rows=1>required quests</textarea>"+
+	"<textarea id='category' name='category' rows=1>category</textarea>"+
+	"</div>");
+	CON_LIST.push("c"+CON_COUNTER);
+	CON_COUNTER++;
+	generateGrid();
+	
+	
+			  
+}
+function generateGrid()
+{
+	var builder="";
+	var allex = listToText(QUEST_LIST);
+	var allcx = listToText(CON_LIST);
+	
+		for (j = 0; j < 5; j++) 
+		{
+			for (i = 0; i < 10; i++) 
+			{
+				builder+="<select id='"+i+";;"+j+"' name='box'>"+
+			  "<option value='nil'>nil</option>"+
+			  allex+
+			  allcx+ 
+			  "</select>";
+			}
+			builder+="<br>";
+		}
+	
+	document.getElementById("GridContainer").innerHTML=builder;
+}
+
+function listToText(input)
+{
+	var builder = "";
+	for (i = 0; i < input.length; i++) 
+	{
+		builder+="<option value='"+input[i]+"'>"+input[i]+"</option>";
+	}
+	return builder;
+}
+
+function generateQuestFile()
+{
+	var builder="";
+	var quests = document.getElementsByName("quest");
+	var boxes = document.getElementsByName("box");
+	for(i = 0; i<boxes.length; i++)
+	{
+		var entry = document.getElementById(boxes[i].value);
+		if(boxes[i].value.startsWith("q"))
+		{
+			builder+=entry.children[0].value+";;"
+			+entry.children[1].value+";;"
+			+entry.children[2].value+";;"
+			+entry.children[3].value+";;"
+			+entry.children[4].value+";;"
+			+entry.children[5].value+";;"
+			+boxes[i].id+";;"
+			+entry.children[6].value+";;"
+			+entry.children[7].value+"\n";
+		}
+		if(boxes[i].value.startsWith("c"))
+		{
+			builder+="!"
+			+entry.children[0].value.substring(1)+";;"
+			+boxes[i].id+";;"
+			+entry.children[1].value+";;"
+			+entry.children[2].value+"\n";
+			
+		}
+		
+	}
+	document.getElementById("output").innerHTML=builder;
+}
+
+addQuestContainer();
